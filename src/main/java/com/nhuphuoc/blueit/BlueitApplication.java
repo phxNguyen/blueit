1cc7ed8fa37a181ec174341059a45206e78ab4ae
phxNguyen <nhuphuoc3008@hotmail.com>
2022-08-01 13:49:39 +0700

    init commit

---
+++ src/main/java/com/nhuphuoc/blueit/BlueitApplication.java
@@ -0,0 +1,13 @@
+package com.nhuphuoc.blueit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlueitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlueitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nhuphuoc/blueit/BlueitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhuphuoc.blueit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlueitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
