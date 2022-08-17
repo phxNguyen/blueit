2dc3a2a4ff4a66aec80f37eb1bfcf88bc4f934e3
phxNguyen <nhuphuoc3008@hotmail.com>
2022-08-17 16:52:07 +0700

    implement user detail service

--- src/main/java/com/nhuphuoc/blueit/security/SecurityConfiguration.java
+++ src/main/java/com/nhuphuoc/blueit/security/SecurityConfiguration.java
@@ -2,14 +2,23 @@ package com.nhuphuoc.blueit.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
+
+    private UserDetailsServiceImpl userDetailsService;
+
+    public SecurityConfiguration(UserDetailsServiceImpl userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         http
@@ -20,5 +29,11 @@ public class SecurityConfiguration {
             .formLogin();
         return http.build();
     }
+
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+        auth.userDetailsService(userDetailsService);
+    }
+
+
 
 }
---
+++ src/main/java/com/nhuphuoc/blueit/security/UserDetailsServiceImpl.java
@@ -0,0 +1,33 @@
+package com.nhuphuoc.blueit.security;
+
+import com.nhuphuoc.blueit.entity.User;
+import com.nhuphuoc.blueit.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import javax.swing.text.html.Option;
+import java.util.Optional;
+
+@Service
+public class UserDetailsServiceImpl  implements UserDetailsService {
+
+
+    private UserRepository userRepository;
+
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Optional<User> user  = userRepository.findByEmail(email);
+
+        if(!user.isPresent()){
+            throw  new UsernameNotFoundException(email);
+        }
+
+        return user.get();
+    }
+}
